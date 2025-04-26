590f19ec2e9aff32dcbd704b6720fd37a6565daa
Nightware <l.krocek@gmail.com>
2025-04-26 10:38:08 +0200

    Disable Copy-paste gadget #111

--- modpack/overrides/kubejs/server_scripts/universum/buildinggadgets2.js
+++ modpack/overrides/kubejs/server_scripts/universum/buildinggadgets2.js
@@ -5,4 +5,6 @@ console.info('Hello, World! (Loaded server example script)')
 // disabled item
 ItemEvents.modifyTooltips(event => {
 	event.add('buildinggadgets2:gadget_exchanging', Text.red('---- Disabled ----'));
+	event.add('buildinggadgets2:gadget_cut_paste', Text.red('---- Disabled ----'));
+	event.add('buildinggadgets2:gadget_copy_paste', Text.red('---- Disabled ----'));
 })
